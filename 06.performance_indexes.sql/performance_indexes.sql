56075f1e8c298ddb98fbbff085058fb0785f3c06
Tinyiko Patience Mathebula <tkaymaheso@gmail.com>
2026-02-22 15:32:18 +0200

    Add files via upload

---
+++ 06.performance_indexes.sql/performance_indexes.sql
@@ -0,0 +1,3 @@
+CREATE INDEX idx_enrollments_person ON enrollments(person_id);
+CREATE INDEX idx_enrollments_program ON enrollments(program_id);
+CREATE INDEX idx_enrollments_status ON enrollments(status_id);
